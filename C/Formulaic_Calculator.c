5b704b50d6c87b465e75193bf288fd260a743838
Abhi Kafle <akafle848@gmail.com>
2022-11-13 20:39:51 +0545

    New line

---
+++ C/Formulaic_Calculator.c
@@ -0,0 +1,16 @@
+//This is a program to calculate various formulaes that form the backbone of
+//much of mathematical understanding of things. 
+
+//I suppose we can start with a simple enough considerations and units.
+#include <stdio.h>
+ 
+int main() {
+    float mass, acc, force;
+    force = 0;
+    mass = 54;
+    acc = 9.07;
+    while (mass < acc) {
+        force = mass * acc;
+        printf( "%f\n ",force);
+    }
+}
