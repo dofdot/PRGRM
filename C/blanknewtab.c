0e2f58aa11dbc7731e666c7727061385269bdca2
Abhi Kafle <akafle848@gmail.com>
2022-12-03 13:02:11 +0545

    3.12.2022

--- C/blanknewtab.c
+++ C/blanknewtab.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
 int main(){
-    int c, nl, tb, blnk;
-    while ( 
+		int c, nl, tb, blnk;
+		nl = 0;
+		tb = 0;
+		blnk = 0;
+				while ((c = getchar()) != EOF)
+						if ( c == '\n' ){
+							++nl;
+						}
+						else if ( c == ' ' ){
+							++blnk;
+						}
+						else if ( c == '\t' ){
+							++tb;
+						}
+		printf("%d\n%d\n%d", nl, blnk, tb);
+}
+		
+				
---
+++ C/inputtooutput.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* This program will copy its input to its output replacing each string with one
+ * or more blanks into a single blank */
+
+int main(void){
+    int c;
+    
+    
+}
